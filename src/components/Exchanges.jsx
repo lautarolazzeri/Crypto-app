c83373695a7ce56382ac24326a702bab188d92e7
Lautaro <lautyylazzerii@gmail.com>
2022-01-20 17:24:34 -0300

    exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+import {Row, Col } from 'antd';
 
-import { useGetExchangesQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
-const { Text } = Typography;
-const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
-  if (isFetching) return <Loader />;
-
   return (
     <>
       <Row>
